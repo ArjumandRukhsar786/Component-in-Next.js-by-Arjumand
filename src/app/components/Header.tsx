449956ac9e4ffbe8c65594a50c0c2936cebfc071
Arjumand Rukhsar <162131925+ArjumandRukhsar786@users.noreply.github.com>
2024-10-05 13:03:58 +0500

    next js

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div className='bg-teal-300'>
+    <h1 className='text-center'>This is Header</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ipsam similique doloribus nostrum laudantium quaerat quia eveniet. Accusamus numquam ad id, asperesse sequi quia neque ratione?</p>
+    </div>
+    )
+}
+
+export default Header
